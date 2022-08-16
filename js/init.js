10ab664d0b76ff098f480b68913654817a65b79c
kokoa_kira <chunyuandesu@gmail.com>
2022-08-16 19:50:15 +0800

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
+  var kawaiiconsole = String("✦ To create a world！✦\n          ✧ Fishing Craft Website ✧\n         - Powered by kokoa_kira")
+  
+  
+  console.log('%c%s', 'color: #fb7299', kawaiiconsole);
